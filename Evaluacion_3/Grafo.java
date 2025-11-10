d9475a67655b76f51169442e864953b5dc7eb5a7
andresG211118 <duvanandresm3445@gmail.com>
2025-11-10 19:48:31 +0000

    d

---
+++ Evaluacion_3/Grafo.java
@@ -0,0 +1,19 @@
+public class Grafo {
+     
+    private final int[][] matrizAdyacencia;
+    private final int vertices;
+
+    public Grafo(int[][] matrizAdyacencia) {
+        this.matrizAdyacencia = matrizAdyacencia;
+        this.vertices = matrizAdyacencia.length;
+    }
+
+    public int[][] getMatrizAdyacencia() {
+        return matrizAdyacencia;
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+}
+
